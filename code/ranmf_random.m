40d1684b473fb9d180402f0acc55c562f082e5c4
alitosyali <aliitosyali@gmail.com>
2019-01-10 10:11:23 -0500

    Update ranmf_random.m

--- code/ranmf_random.m
+++ code/ranmf_random.m
@@ -7,8 +7,8 @@ clc
 [parentdir,~,~] = fileparts(pwd);
 
 % % benchmark
-% community = load(fullfile(parentdir, '\benchmark\community1.dat'));
-% edges = load(fullfile(parentdir, '\benchmark\network1.dat'));
+% community = load(fullfile(parentdir, 'benchmark/community3.dat'));
+% edges = load(fullfile(parentdir, 'benchmark/network3.dat'));
 % true_label = community(:,2);
 % N = size(community,1);
 % A = zeros(N,N);
@@ -17,12 +17,12 @@ clc
 % end
 % r = length(unique(true_label));
 % num_iter = 20;
-% num_runs = 100;
+% num_runs = 20;
 % lambda = 0.1;
 
 % % pcn
 % A = load(fullfile(parentdir, '/data/pcn_adj_mat.txt'));
-% r = 20;
+% r = 15;
 % lambda = 0.1;
 % num_iter = 100;
 % num_runs = 100;
@@ -51,13 +51,13 @@ clc
 % lambda = 1;
 % r = 5;
 
-% texas
-A = load(fullfile(parentdir, '/data/texas_adj_mat.txt'));
-true_label = load(fullfile(parentdir, '/data/texas_labels.txt'));
-num_iter = 100;
-num_runs = 100;
-lambda = 3220;
-r = 5;
+% % texas
+% A = load(fullfile(parentdir, '/data/texas_adj_mat.txt'));
+% true_label = load(fullfile(parentdir, '/data/texas_labels.txt'));
+% num_iter = 100;
+% num_runs = 100;
+% lambda = 3220;
+% r = 5;
 
 tic;
 % number of nodes
@@ -65,12 +65,12 @@ n = size(A,1);
 
 %% similarity matrix
 
-% % cosine
-% [S] = cosine_similarity(A);
+% cosine
+[S] = cosine_similarity(A);
 
-% katz
-beta = 0.1;
-[S] = citation_similarity(A, beta);
+% % katz
+% beta = 0.1;
+% [S] = citation_similarity(A, beta);
 
 % % adjacency
 % S = A + A' + eye(n);
@@ -82,8 +82,8 @@ D = diag(sum(S,2));
 L = D - S;
 
 objective = zeros(num_runs,num_iter);
-% quality_score = zeros(1,num_runs);
-quality_score = zeros(3,num_runs);
+quality_score = zeros(1,num_runs);
+% quality_score = zeros(3,num_runs);
 
 for run = 1:num_runs
     fprintf('----------- Run: %d ----------\n', run)
@@ -114,22 +114,25 @@ for run = 1:num_runs
     W = X_initial/E;
     [~,predict_label] = max(W,[],2);
     
-    %     quality_score(run) = QFDistBased(predict_label, A);
-    %     quality_score(run) = db_index(A, predict_label);
+%     quality_score(run) = QFDistBased(predict_label, A);
+%     quality_score(run) = db_index(A, predict_label);
     
-    quality_score(1, run) = PSJaccard(predict_label, true_label); % jaccard
-    quality_score(2, run) = PSNMI(predict_label, true_label); % nmi
-    predicted = bestMap(true_label, predict_label);
-    quality_score(3, run) = sum(predicted == true_label)/length(predicted); % accuracy
+        quality_score(1, run) = PSJaccard(predict_label, true_label); % jaccard
+        if length(unique(predict_label)) == length(unique(true_label))
+            quality_score(2, run) = PSNMI(predict_label, true_label); % nmi
+        end
+        predicted = bestMap(true_label, predict_label);
+        quality_score(3, run) = sum(predicted == true_label)/length(predicted); % accuracy
 end
 toc;
-% for pcn
+
+% % for pcn
 % mean(quality_score)
-% table(mean(quality_score), 'RowNames', {'RANMF_Rand_Cosine'})
+% table(mean(quality_score), 'RowNames', {'RANMF_Rand'})
 
 % for others
 jaccard = mean(quality_score(1, :));
-nmi = mean(quality_score(2, :));
+nmi = mean(nonzeros(quality_score(2, :)));
 accuracy = mean(quality_score(3, :));
 table(jaccard, nmi, accuracy, 'RowNames', {'RANMF'})
 
