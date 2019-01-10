5359af9b3927e8bd67651cdb1252d13dd8b07290
alitosyali <aliitosyali@gmail.com>
2019-01-10 10:12:11 -0500

    Update ranmf_svd.m

--- code/ranmf_svd.m
+++ code/ranmf_svd.m
@@ -7,8 +7,8 @@ clc
 [parentdir,~,~] = fileparts(pwd);
 
 % % benchmark
-% community = load(fullfile(parentdir, '/benchmark/community2.dat'));
-% edges = load(fullfile(parentdir, '/benchmark/network2.dat'));
+% community = load(fullfile(parentdir, '/benchmark/community3.dat'));
+% edges = load(fullfile(parentdir, '/benchmark/network3.dat'));
 % true_label = community(:,2);
 % N = size(community,1);
 % A = zeros(N,N);
@@ -16,19 +16,19 @@ clc
 %     A(edges(i,1),edges(i,2)) = 1;
 % end
 % r = length(unique(true_label));
-% lambda = 0.2;
+% lambda = 0.1;
 % num_iter = 800;
 
 % % pcn: 1/maxeig 1/0
 % A = load(fullfile(parentdir, '/data/pcn_adj_mat.txt'));
-% r = 7;
+% r = 15;
 % lambda = 0.1;
-% num_iter = 200;
+% num_iter = 50;
 
 % % cornell: 1/maxeig 0.5952
 % A = load(fullfile(parentdir, '/data/cornell_adj_mat.txt'));
 % true_label = load(fullfile(parentdir, '/data/cornell_labels.txt'));
-% num_iter = 200;
+% num_iter = 100;
 % lambda = 10;
 % r = 5;
 
@@ -46,12 +46,12 @@ clc
 % lambda = 1;
 % r = 5;
 
-% texas: 1/maxeig 0.4343
-A = load(fullfile(parentdir, '/data/texas_adj_mat.txt'));
-true_label = load(fullfile(parentdir, '/data/texas_labels.txt'));
-num_iter = 100;
-lambda = 3220;
-r = 5;
+% % texas: 1/maxeig 0.4343
+% A = load(fullfile(parentdir, '/data/texas_adj_mat.txt'));
+% true_label = load(fullfile(parentdir, '/data/texas_labels.txt'));
+% num_iter = 100;
+% lambda = 3220;
+% r = 5;
 
 tic;
 % number of nodes
@@ -59,12 +59,12 @@ n = size(A,1);
 
 %% similarity matrix
 
-% cosine
-[S] = cosine_similarity(A);
+% % cosine
+% [S] = cosine_similarity(A);
 
-% % katz
-% beta = 0.2
-% [S] = citation_similarity(A, beta);
+% katz
+beta = 0.01;
+[S] = citation_similarity(A, beta);
 
 % % adjacency
 % S = A + A' + eye(n);
@@ -109,7 +109,7 @@ toc;
 % % for pcn
 % quality_score = QFDistBased(predict_label, A);
 % [db_score] = db_index(A, predict_label);
-% table(db_score, 'RowNames', {'RANMF'})
+% table(db_score,quality_score, 'RowNames', {'RANMF'})
 
 % for others
 jaccard = PSJaccard(predict_label, true_label); % jaccard
